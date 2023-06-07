b8edc8f93a989a32e9518d0e80e20394b279f16f
vishalgddam873 <vishal18@navgurukul.org>
2023-06-07 15:56:21 +0530

    Code updated

--- main.py
+++ main.py
@@ -6,94 +6,82 @@ from cvzone.HandTrackingModule import HandDetector
 from cvzone.FaceMeshModule import FaceMeshDetector
 
 try:
-    # Capture the camera feed and set the resolution
     cap = cv2.VideoCapture(0)
     cap.set(3, 1280)
     cap.set(4, 720)
-
-    # Create list to hold face images
-    faceImages = []
-    path = "Images/"
+    filterImages =[]
+    path = "images"
     pathList = os.listdir(path)
-    pathList.sort()
 
-    # Load all the images in the list
-    for x, pathImg in enumerate(pathList):
+    for pathImg in pathList:
         img = (cv2.imread(path+"/"+pathImg, cv2.IMREAD_UNCHANGED))
-        img = cv2.resize(img, (100, 100))
-        faceImages.append(img)
+        filterImages.append(img)
 
-    # Creating object to detect hand and face
     detector = HandDetector(detectionCon=0.8)
+
+    # Creating object to detect the face
     faceDetector = FaceMeshDetector(maxFaces=2)
 
 except Exception as e:
     print(e)
-
-
-# Function to place objects on the face
-def showObjectOnface(backImg, frontImg, xLoc, yLoc, dist, rf, rx, ry):
-    resizefactor = dist/rf
-    frontImg = cv2.resize(frontImg, (0, 0), fx=resizefactor, fy=resizefactor)
-    backImg = cvzone.overlayPNG(backImg, frontImg, [int(
-        xLoc - (resizefactor*rx)), int(yLoc - (resizefactor * ry))])
-    return backImg
-
-
-# Loop to display video
+    
+   
 while True:
     try:
-        # Get a single capture from the camera
         success, cameraFeedImg = cap.read()
-
-        cameraFeedImg = cv2.resize(cameraFeedImg, (640, 480))
+        
+        cameraFeedImg= cv2.resize(cameraFeedImg, (640, 480))
         cameraFeedImg = cv2.flip(cameraFeedImg, 1)
 
-        # Get width and height of final output screen
         wHeight, wWidth, wChannel = cameraFeedImg.shape
-
+        
         # Detecting face in the cameraFeedImg
-        faces = False
+        cameraFeedImg, faces = faceDetector.findFaceMesh(cameraFeedImg, draw= True) 
 
-        cameraFeedImg, faces = faceDetector.findFaceMesh(
-            cameraFeedImg, draw=True)
-
-        for face in faces:
-            xLoc = face[21][0]
-            yLoc = face[21][1]
-
+        # Loop over each face in the faces
+        for face in faces: 
+            # Get x and y coordinates of face[21] landmark and save them in xLoc and yLoc variables
+            xLoc= face[21][0]
+            yLoc= face[21][1]   
+                
+            # Calculate and store distance between face[21] and face[251] landmakr i.e width of the face    
             dist = math.dist(face[21], face[251])
+            # Set initial scale to 55, dx to 25 and dy to 35
             scale = 55
             dx = 25
-            dy = 50
-
-            # Distance between 13, 14 is mouth open distance
-            # Distance between 76, 106 lip ends
+            dy = 35
+ 
+            # Distance between 13, 14 is mouth open distance, store it in lipEndDistance variable
+            # Distanvce between 76, 106 lip ends, store it in lipOpenDistance variable
             lipEndDistance = math.dist(face[76], face[306])
             lipOpenDistance = math.dist(face[13], face[14])
+            
+            # Create variable filterImage and assign filterImages[0] to act as initial filter image
+            filterImage = filterImages[0]
 
-            faceImage = faceImages[0]
-            print(dist/lipEndDistance)
-            for i in range(0, 450):
-
-                if i == 13 or i == 14 or i == 43 or i == 306 or i == 76:
-                    cameraFeedImg = cv2.putText(cameraFeedImg, str(i), (face[i][0], face[i][1]), cv2.FONT_HERSHEY_SIMPLEX,
-                                                0.3, (255, 0, 0), 1, cv2.LINE_AA)
-
+            # Check if lipOpenDistance < 10 and set filterImage to filterImages[0]             
             if lipOpenDistance < 10:
-                faceImage = faceImages[0]
+                filterImage = filterImages[0]
+            # Else set filterImage to filterImages[1]    
             else:
-                faceImage = faceImages[1]
+                filterImage = filterImages[1]
 
-            if (dist/lipEndDistance < 2.5):
-                faceImage = faceImages[2]
+            # Check if dist/lipEndDistance < 2.5 and set filterImage to filterImages[2]  
+            if(dist/lipEndDistance < 2.5):
+                filterImage = filterImages[2]
+            
+            # Calculate resizefactor as dist/scale
+            resizefactor = dist/scale
 
-            cameraFeedImg = showObjectOnface(
-                cameraFeedImg, faceImage, xLoc, yLoc, dist, scale, dx, dy)
+            # Resize filterImage to standard size of 100,100 
+            filterImage= cv2.resize(filterImage, (100, 100))
+            # Resize filterImage to face size using resizefactor
+            filterImage= cv2.resize(filterImage, (0, 0), fx = resizefactor, fy = resizefactor)
+            # Show filterimage on cameraFeedImg at int(xLoc - (resizefactor*dx)) for x and int(yLoc - (resizefactor*dy)) for y axis
+            cameraFeedImg= cvzone.overlayPNG(cameraFeedImg, filterImage, [int(xLoc - (resizefactor*dx)), int(yLoc - (resizefactor * dy))])
 
     except Exception as e:
-        print("Exception", e)
+        print("Exception", e) 
 
-    # Show final image
     cv2.imshow("Image", cameraFeedImg)
     cv2.waitKey(1)
